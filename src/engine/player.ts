50997ac7be80974830363e97ac100f877dd01f5b
ckieverentineas <ckieverentineas@yandex.ru>
2023-01-29 19:24:03 +1000

    added new parser for answers

--- src/engine/player.ts
+++ src/engine/player.ts
@@ -36,6 +36,18 @@ async function Book_Random_String_Helper(filename: string) {
         console.log(err);
     }
 }
+async function Book_Random_String_Helper_Mod(filename: string) {
+    try {
+        const contents = await fsPromises.readFile(filename, 'utf-8');
+        const arr: Array<string> = contents.split(/\n\s*\n/)
+        //const arr: any = contents.split(/\r?\n/);
+        const clear = arr.filter((value: any) => value !== undefined && value.length > 1);
+        console.log(`Обнаружено количество предложений: ${clear.length}`)
+        return clear;
+    } catch (err) {
+        console.log(err);
+    }
+}
 async function Book_Random_Word(arr_sentence: Array<string>, context: any, name_book: string) {
     try {
         const data_old = Date.now()
@@ -87,7 +99,7 @@ async function Book_Random_Dictionary(arr_sentence: Array<string>, context: any,
             //const arr: Array<string> = await Az.Tokens(arr_sentence[i]).done();
             //const arr: Array<string> = arr_sentence[i].toLowerCase().replace(/[^а-яА-Я ]/g, "").split(/(?:,| )+/)
             const temp = arr.filter((value: any) => value !== undefined && value.length > 0);
-            for (let j = 0; j < temp.length-1; j++) {
+            for (let j = 0; j < temp.length; j++) {
                 const one = temp[j].toLowerCase()
                 try {
                     if (!one) {continue}
@@ -145,6 +157,40 @@ async function Book_Random_Question(arr_sentence: Array<string>, context: any, n
         console.log(err);
     }
 }
+async function Book_Random_Question_Mod(arr_sentence: Array<string>, context: any, name_book: string) {
+    try {
+        const data_old = Date.now()
+        console.log(`Переданно предложений: ${arr_sentence.length}`)
+        let count = 0
+        let count_circle = 0
+        for (const i in arr_sentence) {
+            const arr: Array<string> = arr_sentence[i].split(/\r\n/)
+            //const arr: Array<string> = await Az.Tokens(arr_sentence[i]).done();
+            //const arr: Array<string> = arr_sentence[i].toLowerCase().replace(/[^а-яА-Я ]/g, "").split(/(?:,| )+/)
+            const temp = arr.filter((value: any) => value !== undefined && value.length > 0);
+            for (let j = 0; j < temp.length-1; j++) {
+                const word1 = temp[j].toLowerCase()
+                const word2 = temp[j+1].toLowerCase()
+                try {
+                    const first: any = await prisma.answer.findFirst({ where: { qestion: word1, answer: word2 }, select: {id: true}})
+                    if (!first) {
+                        const create = await prisma.answer.create({ data: { qestion: word1, answer: word2 }})
+                        console.log(`Add new question experimental: ${create.id} - ${create.qestion} > ${create.answer}`)
+                        count++
+                    }
+                } catch (err) {
+                    console.log(`Ошибка ${err}`)
+                }
+                
+                count_circle++
+            }
+        }
+        console.log(`Read couple: ${count_circle}, Set new couple: ${count}`)
+        await context.send(`✅ Список вопросов экспериментальный: ${name_book} Найдено вопрсов: ${count_circle}, Добавлено ответов: ${count} Затраченно времени: ${(Date.now() - data_old)/1000} сек.`)
+    } catch (err) {
+        console.log(err);
+    }
+}
 async function Move_Book(dir:string, file:string) {
     await fs.copyFile(`${dir}/${file}`, `${dir}/done/${file}`, COPYFILE_EXCL)
     await fs.unlink(`${dir}/${file}`)
@@ -166,6 +212,11 @@ async function MultipleReaderQuestion(dir:string, file:string, context: any) {
     const arr: Array<string> = await Book_Random_String_Helper(`${dir}/${file}`) || []
     await context.send(`Создаем списки вопросов и ответов к ним: ${file}, строк: ${arr.length}`)
     await Book_Random_Question(arr, context, file)
+}
+async function MultipleReaderQuestionMod(dir:string, file:string, context: any) {
+    const arr: Array<string> = await Book_Random_String_Helper_Mod(`${dir}/${file}`) || []
+    await context.send(`Создаем списки вопросов и ответов к ним другого формата: ${file}, строк: ${arr.length}`)
+    await Book_Random_Question_Mod(arr, context, file)
 }
 export function registerUserRoutes(hearManager: HearManager<IQuestionMessageContext>): void {
     hearManager.hear(/syscalledu/, async (context) => {
@@ -193,6 +244,30 @@ export function registerUserRoutes(hearManager: HearManager<IQuestionMessageCont
             for (const file of file_name) {
                 await MultipleReaderQuestion(dir, file, context)
             }
+        }
+    })
+    hearManager.hear(/syscallother/, async (context) => {
+        if (context.isOutbox == false && context.senderId == root) {
+            const dir = `./src/book`
+            const file_name: any = await readDir(dir)
+            for (const file of file_name) {
+                await MultipleReaderQuestionMod(dir, file, context)
+            }
+        }
+    })
+    hearManager.hear(/syscallconfig/, async (context) => {
+        if (context.isOutbox == false && context.senderId == root) {
+            await context.send(`Панель администратора: \n 👤 Личные сообщения: Разрешены \n 👥 Беседы: Разрешены \n ⚙ Защиты отсуствуют`)
+        }
+    })
+    hearManager.hear(/syscallhelp/, async (context) => {
+        if (context.isOutbox == false && context.senderId == root) {
+            await context.send(`☠ Команды бота уже сделанные:
+            \n⚙ syscalldict (словарь) - пополняет словарный запас бота на все еще не встреченные слова до этого, нужен для нечеткого поиска в базе данных и становления связей*
+            \n⚙ syscalledu (обучение) - устанавливает парные связи слов на основе существующего словаря и чтения книг*
+            \n⚙ syscallbase (вопрос-ответ) - считывает тхт формата: Вопрос\Ответ и все что до второй , остальное нам нафиг не надо. закидывая вопрос-ответы в базу данных
+            \n💡 По пути ./src/book/ кладем в директорию (папку) книгу/answer_database в txt формата, и вначале выполняем команду словарь, по ее окончанию обучение.
+            \n💡 Примечание: 1 МБ txt считывается 4+ часа, т.е. при загрузки 1 МБ тхт документа потребуется 4 часа на пополнение словарного запаса, и еще 4 для установления связей на основе полученных слов и их последовательности в книге. А при считывании вопрос-ответ базы данных 6-7 строк в секунду.`)
         }
     })
 }
