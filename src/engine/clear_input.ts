eb8aba8cca2ce779d1d6e0852387d66cd48a948b
ckieverentineas <ckieverentineas@yandex.ru>
2024-06-28 17:40:26 +1000

    some fixed clear input

--- src/engine/clear_input.ts
+++ src/engine/clear_input.ts
@@ -1,6 +1,6 @@
 export function Input_Message_Cleaner(text: string) {
 	try {
-		return text.replace(/[^а-яА-Я0-9ёЁ \-+—–_•()/\\"'`«»{}[#№\]=:;.,!?...\n\r]/gi, '')
+		return text.replace(/[^а-яА-Я0-9ёЁ \-+—–•()"'`«»#№=:;.,!?...\n\r]/gi, '')
 	} catch {
 		return ' '
 	}
